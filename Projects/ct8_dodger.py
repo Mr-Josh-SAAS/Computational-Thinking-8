a98e41187ad8c00fa03983bea86f0e97c58deb26
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2024-08-21 01:45:33 +0000

    event_interval working

--- Projects/ct8_dodger.py
+++ Projects/ct8_dodger.py
@@ -1,6 +1,7 @@
 #### Setup ####
 import codesters
 stage.set_background("moon")
+stage.disable_floor()
 bird = codesters.Sprite("cardinal")
 bird.set_size(0.5)
 bird.go_to(0,-200)
@@ -31,27 +32,30 @@ bird.event_key("left", go_left)
 
 #### Asteroids ####
 
-# def asteroid_fall():
-#     global playing
-#     if playing:
-#         x_position = random.randint(-250,250)
-#         asteroid = codesters.Sprite("cat", x_position, 250)
-#         asteroid.set_size(0.4)
-#         asteroid.move_down(600)
+def asteroid_fall():
+    global playing, lives
+    if playing:
+        x_position = random.randint(-250,250)
+        asteroid = codesters.Sprite("rock", x_position, 300)
+        # asteroid.set_size(0.4)
+        # asteroid.move_down(600)
+        asteroid.set_y_speed(-10)
+        # print("test")
+        print(lives)
     
-# stage.event_interval(asteroid_fall, 0.5)
+stage.event_interval(asteroid_fall, 5)
 
 # Collision
 
 # def collision(s1, s2):
 #     global lives
-#     if s2.get_image_name() == "cat":
-#         lives -= 1
-#         lives_display.update(lives)
-#         stage.remove_sprite(s2)
-#         if lives == 0:
-#             global playing
-#             playing = False
+# #     if s2.get_image_name() == "rock":
+#     lives -= 1
+# # #         lives_display.update(lives)
+# # #         stage.remove_sprite(s2)
+# # #         if lives == 0:
+# # #             global playing
+# # #             playing = False
         
 # bird.event_collision(collision)
 
