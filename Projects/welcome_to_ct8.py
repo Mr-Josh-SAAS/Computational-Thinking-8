b5058a2842519c26bca292fa54e785d0f375a5d9
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2024-08-27 09:36:49 -0700

    Import modules (#8)
    * lunch attempts
    
    * incantation version
    
    * hide __pycache__

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -1,4 +1,11 @@
+###############################################
+### SETUP ###
 import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+
 stage.set_background("fall")
 mySprite = codesters.Sprite("cardinal")
 mySprite.say("Good job finding me!")
