ea9c62b2e97c1ac3f513c395706737f14039732b
hornsilk <hornsilk01@gmail.com>
2024-09-05 14:59:45 +0000

    scratchwork 9/5

---
+++ Projects/ct8_p1_coat_of_arms.py
@@ -0,0 +1,12 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+
+q1 = codesters.Square(100, 100, 200, 'red')
+q2 = codesters.Square(-100, 100, 200, 'yellow')
+q3 = codesters.Square(-100, -100, 200, 'red')
+q4 = codesters.Square(100, -100, 200, 'green')
