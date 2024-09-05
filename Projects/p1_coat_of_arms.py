b7013b5ee1071cbfffcdc08ec53bf1d3ebc665a7
hornsilk <hornsilk01@gmail.com>
2024-09-05 15:47:38 +0000

    added triangles

--- Projects/p1_coat_of_arms.py
+++ Projects/p1_coat_of_arms.py
@@ -5,12 +5,15 @@ from codesters import StageClass
 stage = StageClass()
 ###############################################
 
+# https://en.wikipedia.org/wiki/Web_colors#Extended_colors
+
 stage.set_background("winter")
 
-q1 = codesters.Square(100, 100, 200, 'red')
-q2 = codesters.Square(-100, 100, 200, 'yellow')
+q1 = codesters.Square(100, 100, 200, 'black')
+q2 = codesters.Square(-100, 100, 200, 'Gold')
 q3 = codesters.Square(-100, -100, 200, 'red')
 q4 = codesters.Square(100, -100, 200, 'pink')
+t1 = codesters.TriangleRight(100,100,200,200,"red")
 
 s1 = codesters.Sprite("cardinal", 100, 100)
 s2 = codesters.Sprite("computer", -100, -100)
@@ -21,4 +24,4 @@ s4 = codesters.Sprite("roof", -100, 100)
 s4.set_size(0.1)
 
 message1 = codesters.Text("Joshua Tyler Kurisko",0,220,"red")
-message1 = codesters.Text("My House, My Rules, My Coffee",0,-220,"yellow")
+message2 = codesters.Text("My House, My Rules, My Coffee",0,-220,"DarkSlateBlue")
