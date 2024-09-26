a1d3fba916bda92f38c615e90203e08272fd8c60
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2024-09-26 16:56:09 +0000

    example

--- Projects/if_statements.py
+++ Projects/if_statements.py
@@ -1,18 +1,11 @@
-###############################################
-### SETUP ###
-import codesters
-from codesters import StageClass
-stage = StageClass()
-###############################################
 word = input("What do you think grandma likes? ")
-
 
 if "t" in word:
     print("Grandma doesn't like t")
 
 else:
-    grandma_sprite = codesters.Sprite("grandma")
-    grandma_sprite.say("I like that!")
+    print(f"Grandma likes {word}")
+    print("Good Job")
 
 
 
