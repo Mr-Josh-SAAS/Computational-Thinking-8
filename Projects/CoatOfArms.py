7fe3be1e72735fc364d39ddcc86b283391c24409
JeNeSaisPasJJ <theosterling@seattleacademy.org>
2025-03-18 17:29:20 +0000

    submitting P1

---
+++ Projects/CoatOfArms.py
@@ -0,0 +1,22 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+
+stage.set_background("footballfield")
+q4 = codesters.Square(-100, 100, 200, 'green')
+q1 = codesters.Square(100 , 100 , 200, 'blue')
+q3 = codesters.Square(-100, -100, 200, 'red')
+q2 = codesters.Square(100, -100, 200, 'yellow')
+mySprite = codesters.Sprite("seahawk" , -100 , 100)
+mySprite.set_size(0.75)
+mySprite.say("12th man")
+mySprite2 = codesters.Sprite("planetzebulon" , 100 , 100)
+mySprite2.set_size(0.75)
+mySprite3 = codesters.Sprite("odea2" , -100, -100)
+mySprite3.set_size(0.8)
+mySprite4 = codesters.Sprite("football_lax" , 100, -100)
+mySprite4.set_size(0.6)
