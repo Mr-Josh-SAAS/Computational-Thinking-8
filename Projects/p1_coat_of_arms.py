3ae4a6e31997b835648729be4b22475d3d7d0519
hornsilk <hornsilk01@gmail.com>
2025-03-20 18:31:27 +0000

    turning in project 1

--- Projects/p1_coat_of_arms.py
+++ Projects/p1_coat_of_arms.py
@@ -7,7 +7,7 @@ stage = StageClass()
 
 # https://en.wikipedia.org/wiki/Web_colors#Extended_colors
 
-stage.set_background("winter")
+stage.set_background("saas")
 
 q1 = codesters.Square(100, 100, 200, 'black')
 q2 = codesters.Square(-100, 100, 200, 'Gold')
@@ -18,10 +18,11 @@ t1 = codesters.TriangleRight(100,100,200,200,"red")
 s1 = codesters.Sprite("cardinal", 100, 100)
 s2 = codesters.Sprite("computer", -100, -100)
 s2.set_size(0.5)
-s3 = codesters.Sprite("knitting", 100, -100)
-s3.set_size(0.51)
-s4 = codesters.Sprite("roof", -100, 100)
-s4.set_size(0.1)
+s3 = codesters.Sprite("pineapple", 100, -100)
+s3.set_size(0.7)
+
+s4 = codesters.Sprite("rock_climbers", -100, 100)
+s4.set_size(0.5)
 
 message1 = codesters.Text("Joshua Tyler Kurisko",0,220,"red")
 message2 = codesters.Text("My House, My Rules, My Coffee",0,-220,"DarkSlateBlue")
