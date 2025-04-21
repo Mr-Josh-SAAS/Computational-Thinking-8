2f3622f1c9f158f908133e859a69678e00d12190
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2025-04-21 10:54:15 -0700

    rename to turtle racing

---
+++ Projects/p4_turtle_racing_pygame.py
@@ -0,0 +1,61 @@
+import turtle, time, random
+
+# Section 1 - Helper functions
+def set_background(image_filename):
+    screen = turtle.Screen()
+    try:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+    except:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+
+def create_sprite(image_filename, x=0, y=0):
+    image_file = f"/workspaces/Computational-Thinking-8/Images/{image_filename}.gif"
+    screen = turtle.Screen()
+    screen.register_shape(image_file)
+    sprite = turtle.Turtle()
+    sprite.shape(image_file)
+    sprite.penup()
+    sprite.goto(x,y)
+    return sprite
+
+# Section 2 - Variables
+x1 = -200
+y1 = 200
+x2 = -200
+y2 = 100
+x3 = -200
+y3 = 0
+x4 = -200
+y4 = -100
+
+# Section 3 - Setup
+set_background("castle")
+t1 = create_sprite("cardinal2",x1,y1)
+t2 = create_sprite("basketball",x2,y2)
+t3 = create_sprite("basketball",x3,y3)
+t4 = create_sprite("basketball",x4,y4)
+
+# Section 4 - Racing
+for i in range(30):
+    x1 += 10
+    x2 += 15
+    x3 += 10
+    x4 += random.randint(1,50)
+
+    t1.goto(x1, y1)
+    t2.goto(x2, y2)
+    t3.goto(x3, y3)
+    t4.goto(x4, y4)
+    time.sleep(0.05) 
+
+# Section 5 - Winner
+if x1 >= x2 and x1 >= x3 and x1 >= x4:
+    print("player 1 wins!")
+if x2 >= x1 and x2 >= x3 and x2 >= x4:
+    print("player 2 wins!")
+if x3 >= x1 and x3 >= x2 and x3 >= x4:
+    print("player 3 wins!")
+if x4 >= x1 and x4 >= x2 and x4 >= x3:
+    print("player 4 wins!")
+
+turtle.exitonclick()
