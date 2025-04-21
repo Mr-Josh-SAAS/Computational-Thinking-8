e21a7023858f6fe482b9fd03f694618160a69d8c
hornsilk <hornsilk01@gmail.com>
2025-04-21 17:17:33 +0000

    amazing race in pygame

--- Projects/amazing_race_pygame.py
+++ Projects/amazing_race_pygame.py
@@ -1,9 +1,15 @@
 import turtle, time, random
 
-screen = turtle.Screen()
-screen.bgpic("/workspaces/Computational-Thinking-8/Backgrounds/castle.png")
-def create_sprite(image_filename, x, y):
+def set_background(image_filename):
+    screen = turtle.Screen()
+    try:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+    except:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+
+def create_sprite(image_filename, x=0, y=0):
     image_file = f"/workspaces/Computational-Thinking-8/Images/{image_filename}.gif"
+    screen = turtle.Screen()
     screen.register_shape(image_file)
     sprite = turtle.Turtle()
     sprite.shape(image_file)
@@ -26,11 +32,12 @@ x4 = -200
 y4 = -100
 
 # Section 2 - Setup
-
-t1 = create_sprite("basketball",x1,y1)
+set_background("castle")
+t1 = create_sprite("cardinal2",x1,y1)
 t2 = create_sprite("basketball",x2,y2)
 t3 = create_sprite("basketball",x3,y3)
 t4 = create_sprite("basketball",x4,y4)
+
 
 
 # Section 3 - Racing
