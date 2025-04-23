79b5c545209d6490b838e79943839c361898b74d
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2025-04-23 11:03:13 -0700

    more template stuff

--- Projects/p4_turtle_racing_pygame_example_code.py
+++ Projects/p4_turtle_racing_pygame_example_code.py
@@ -6,7 +6,6 @@ def set_background(image_filename):
         screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
     except:
         screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
-
 def create_sprite(image_filename, x=0, y=0):
     image_file = f"/workspaces/Computational-Thinking-8/Images/{image_filename}.gif"
     screen = turtle.Screen()
@@ -17,7 +16,9 @@ def create_sprite(image_filename, x=0, y=0):
     sprite.goto(x,y)
     return sprite
 
+
 # Section 2 - Variables
+# TODO - add starting values for all the variables
 x1 =
 y1 =
 x2 =
@@ -27,30 +28,38 @@ y3 =
 x4 =
 y4 =
 
+
 # Section 3 - Setup
+# TODO - use your own background, and set your four turtles to images of your choice
 set_background("castle")
 t1 = create_sprite("basketball",x1,y1)
 t2 = create_sprite("basketball",x2,y2)
 t3 = create_sprite("basketball",x3,y3)
 t4 = create_sprite("basketball",x4,y4)
 
+
 # Section 4 - Racing
+# TODO - set how much each variable changes by
 for i in range(30):
-    x1 += 
-    x2 += 
-    x3 += 
-    x4 += 
+    x1 +=
+    x2 +=
+    x3 +=
+    x4 +=
+
 
     t1.goto(x1, y1)
     t2.goto(x2, y2)
     t3.goto(x3, y3)
     t4.goto(x4, y4)
-    time.sleep(0.05) 
+    time.sleep(0.05)
+
 
 # Section 5 - Winner
+# TODO - complete the elif for player 2 winning
+# TODO - write another elif for player 3 and player 4
 if x1 >= x2 and x1 >= x3 and x1 >= x4:
     print("player 1 wins!")
-elif 
+elif
     print("player 2 wins!")
 
 
