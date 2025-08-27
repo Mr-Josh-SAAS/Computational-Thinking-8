100debd2f4b61cce31d136d6c9293254a9eb3671
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2025-08-27 11:47:02 -0700

    new welcome program

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -1,16 +1,3 @@
-###############################################
-### SETUP ###
-import codesters
-from codesters import StageClass
-stage = StageClass()
-###############################################
-
-
-stage.set_background("spring")
-mySprite = codesters.Sprite("cardinal")
-mySprite.say("Good job finding me!")
-
-
 print("Welcome to CT8!")
 print("Great job correctly setting up this codespace")
 print("We will be using this site for the majority of the projects for this class")
@@ -19,10 +6,25 @@ input("\n---Press Enter when you have bookmarked the page---\n")
 
 
 print("\n\n")
-print("Now, try to view the display screen")
-print("To view the display screen:")
-print("\t1: click PORTS on the menu bar above this text")
-print("\t2: move your mouse over the words in blue that start with https://")
-print("\t3: a few icons should appear - click the globe")
-print("\t4: a new tab will open - click CONNECT")
-print("\n\nWhen you have found the CARDINAL, click here, then use CTRL C to end the program\n\n")
+print("This area is called the TERMINAL.")
+print("It shows messages and errors.")
+print("You also can type in the terminal.")
+name = input("\n---Type your name here, then press Enter---\n")
+
+print("\n\n")
+print(r"  _____ ____  __  __ _____    _______ _    _ _____ _   _ _  _______ _   _  _____    ___  ")
+print(r" / ____/ __ \|  \/  |  __ \  |__   __| |  | |_   _| \ | | |/ /_   _| \ | |/ ____|  / _ \ ")
+print(r"| |   | |  | | \  / | |__) |    | |  | |__| | | | |  \| | ' /  | | |  \| | |  __  | (_) |")
+print(r"| |   | |  | | |\/| |  ___/     | |  |  __  | | | | . ` |  <   | | | . ` | | |_ |  > _ < ")
+print(r"| |___| |__| | |  | | |         | |  | |  | |_| |_| |\  | . \ _| |_| |\  | |__| | | (_) |")
+print(r" \_____\____/|_|  |_|_|         |_|  |_|  |_|_____|_| \_|_|\_\_____|_| \_|\_____|  \___/ ")
+# text art from https://patorjk.com/software/taag/
+
+
+print(f"This codespace belongs to {name}")
+print("\n\n")
+                                                                                          
+                                                                                          
+                                  
+print("Now it's your turn:")
+print("Find this line (line 28) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
