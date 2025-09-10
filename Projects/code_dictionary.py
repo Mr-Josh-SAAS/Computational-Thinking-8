476f7d2c54ea950a9d84ac5bb3121252fcc546dd
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2025-09-10 11:28:00 -0700

    test submission

---
+++ Projects/code_dictionary.py
@@ -0,0 +1,13 @@
+print("This is my message")
+
+name = input("What is your name? ")
+print(f"Hello {name} and welcome to CT8")
+
+
+
+print(r"  _____ ____  __  __ _____  ")
+print(r" / ____/ __ \|  \/  |  __ \ ")
+print(r"| |   | |  | | \  / | |__) |")
+print(r"| |   | |  | | |\/| |  ___/ ")
+print(r"| |___| |__| | |  | | |     ")
+print(r" \_____\____/|_|  |_|_|     ")
