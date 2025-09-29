c595696a10426a44088da8fa73b2bf17c38ad378
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2025-09-29 09:48:38 -0700

    example programs

---
+++ Projects/quiz_p2.py
@@ -0,0 +1,24 @@
+winter_points = 0
+summer_points = 0
+spring_points = 0
+
+answer = input("Would you rather wear A short sleeves, B long sleeves, or C neither   "
+if answer == "A":
+	summer_points += 1
+	print("great answer!")
+elif answer == "B":
+	winter_points += 1
+elif answer == "C"
+	spring_points += 1
+else:
+	print("please type only A, B, or C")
+
+answer = input("Would you rather A snowboard, B ski, or C swim   ")
+if awnser == "A" or answer == "B":
+	winter_points += 2
+elif answer == "C":
+	summer_points += 1
+    spring_points += 1
+else:
+	print("please type only A, B, or C")
+
