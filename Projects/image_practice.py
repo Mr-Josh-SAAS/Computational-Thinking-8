941bad7e04a8d49871bbf13748686c3862c03c5f
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2025-09-29 09:50:20 -0700

    codesters projects purge

--- Projects/image_practice.py
+++ Projects/image_practice.py
@@ -10,3 +10,28 @@ stage.set_background("winter")
 sprite1 = codesters.Sprite("cardinal", 0, 0)
 sprite1.say("Go SAAS!")
 
+# Section 1 - Helper functions (DON'T CHANGE!!)
+import turtle, math, time, random
+def set_background(image_filename):
+	screen = turtle.Screen()
+	try:
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+	except:
+		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+def set_image(sprite, image_filename):
+	image_file = f"/workspaces/Computational-Thinking-8/Images/{image_filename}.gif"
+	screen = turtle.Screen()
+	screen.register_shape(image_file)
+	sprite.shape(image_file)
+def create_sprite(image_filename, x=0, y=0):
+	sprite = turtle.Turtle()
+	set_image(sprite, image_filename)
+	sprite.penup()
+	sprite.goto(x,y)
+	return sprite
+def get_distance(s1, s2):
+	dx = s1.xcor() - s2.xcor()
+	dy = s1.ycor() - s2.ycor()
+	return math.sqrt(dx*dx + dy*dy)
+window = turtle.Screen()
+window.tracer(0)
