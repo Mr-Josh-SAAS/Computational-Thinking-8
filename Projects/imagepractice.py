3c91e70007ff389d32692a0c2cb6d6bd610ee661
Alanaemy <alanayi@seattleacademy.org>
2025-10-14 01:47:06 +0000

    project 3

---
+++ Projects/imagepractice.py
@@ -0,0 +1,46 @@
+# Section 1 - Helper functions (DON'T CHANGE!!)
+import turtle, math, time, random
+def set_background(image_filename):
+    screen = turtle.Screen()
+    try:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
+    except:
+        screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+
+def set_image(sprite, image_filename):
+    image_file = f"./Images/{image_filename}.gif"
+    screen = turtle.Screen()
+    screen.register_shape(image_file)
+    sprite.shape(image_file)
+
+def create_sprite(image_filename, x=0, y=0):
+    sprite = turtle.Turtle()
+    set_image(sprite, image_filename)
+    sprite.penup()
+    sprite.goto(x,y)
+    window.update()
+    return sprite
+
+def get_distance(s1, s2):
+    dx = s1.xcor() - s2.xcor()
+    dy = s1.ycor() - s2.ycor()
+    return math.sqrt(dx*dx + dy*dy)
+window = turtle.Screen()
+window.tracer(0)
+
+
+######################################################################
+# Section 2 - Your code
+set_background("spaceneedle")
+sprite1 = create_sprite("harry the husky", -200, -100)
+
+sprite2 = create_sprite("alien",-180,150)
+sprite2.color("white")
+sprite2.write("Hi my name is Harry, welcome to my city!",font = ("Arial", 15, "normal"))
+sprite2.hideturtle()
+######################################################################
+
+
+# Section 3 - Keeping the window open (DON'T CHANGE!!)
+window.update()
+turtle.exitonclick()
--- Projects/project2.py
+++ Projects/project2.py
@@ -3,35 +3,35 @@ princess_points=0
 pumpkin_points=0
 
 answer=input ("If people were in a fight would you A get in it, B sing and make everyone happy, or C just watch? ")
-if answer == "A":
+if answer == "A" == "a":
         pirate_points += 1
-elif answer == "B":
+elif answer == "B" == "b":
         princess_points += 1
-elif answer == "C":
+elif answer == "C"  == "c":
         pumpkin_points += 1
 
 answer=input ("Which activity do you like the best A dancing, B digging for gold, or C making coffee? ")
-if answer == "A":
+if answer == "A" == "a":
         princess_points += 2
-elif answer == "B": 
+elif answer == "B" == "b":
         pirate_points += 2
-elif answer == "C":
+elif answer == "C"  == "c":
         pumpkin_points += 2
 
 answer=input ("Where would you like to vacation, A the Bahamas, B a farm, or C Greece? ")
-if answer == "A":
+if answer == "A" or answer == "a":
         pirate_points += 3
-elif answer == "B": 
+elif answer == "B" or answer == "b":
         pumpkin_points += 3
-elif answer == "C":
+elif answer == "C" or answer =="c":
         princess_points += 3
 
 answer=input ("Whats your favorite color, A brown, B yellow, or C pink? ")
-if answer == "A":
+if answer == "A"or answer == "a":
         pumpkin_points += 4
-elif answer == "B": 
+elif answer == "B" or answer == "b":
         pirate_points += 4
-elif answer == "C":
+elif answer == "C" or answer == "c":
         princess_points += 4
 
 answer=input ("Which animal would you want as a pet A a bird, B a dog, or C nothing? ")
@@ -42,11 +42,11 @@ elif answer == "B" or answer == "b":
 elif answer == "C" or answer == "c":
         pumpkin_points += 5
 
-if pirate_points>princess_points and pumpkin_points:
+if pirate_points>princess_points and pirate_points>pumpkin_points:
         print ("You should be a pirate for Halloween")
-elif princess_points>pirate_points and pumpkin_points:
+elif princess_points>pirate_points and princess_points>pumpkin_points:
         print ("You should be a princess for Halloween")
-elif pumpkin_points>pirate_points and princess_points:
+elif pumpkin_points>pirate_points and pumpkin_points>princess_points:
         print ("You should be a pumpkin for Halloween")
 else :print ("You should be all 3!")
 
