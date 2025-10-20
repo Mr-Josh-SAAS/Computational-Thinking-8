6ae2b891450d22bd65789c901a723fb3d5611b89
Alanaemy <alanayi@seattleacademy.org>
2025-10-20 21:12:23 +0000

    Project 4 Submission

---
+++ Projects/project4.py
@@ -0,0 +1,109 @@
+# ###############################################
+# ### SETUP ###
+import turtle
+# ###############################################
+
+#first round
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -100)
+t.color("plum")
+t.pendown()
+
+for i in range(35):
+    t.forward(180)
+    t.left(110)
+
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -100)
+t.color("cyan")
+t.pendown()
+
+for i in range(35):
+    t.forward(190)
+    t.left(110)
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -100)
+t.color("pink")
+t.pendown()
+
+for i in range(35):
+    t.forward(200)
+    t.left(110)
+
+
+##############################
+#second round
+t = turtle.Turtle()
+t.penup()
+t.goto(-50, -100)
+t.color("MediumOrchid")
+t.pendown()
+
+for i in range(35):
+    t.forward(180)
+    t.left(110)
+
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-50, -100)
+t.color("RoyalBlue")
+t.pendown()
+
+for i in range(35):
+    t.forward(190)
+    t.left(110)
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-50, -100)
+t.color("LightCoral")
+t.pendown()
+
+for i in range(35):
+    t.forward(200)
+    t.left(110)
+
+
+##############################
+#third round
+t = turtle.Turtle()
+t.penup()
+t.goto(-25, -100)
+t.color("PaleGreen")
+t.pendown()
+
+for i in range(35):
+    t.forward(180)
+    t.left(110)
+
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-25, -100)
+t.color("LightPink")
+t.pendown()
+
+for i in range(35):
+    t.forward(190)
+    t.left(110)
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-25, -100)
+t.color("Yellow")
+t.pendown()
+
+for i in range(35):
+    t.forward(200)
+    t.left(110)
+
+# ###############################################
+# ### ENDING ###
+turtle.exitonclick()
+# ###############################################
