0e793414f933feed5372d1579a8fc809eef6779b
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2025-11-24 11:49:26 -0800

    update dodge_pygrame to rely on utils.py

--- Projects/dodge_pygame.py
+++ Projects/dodge_pygame.py
@@ -1,33 +1,6 @@
 # Section 1 - helper functions
 import turtle, math, time, random
-def set_background(image_filename):
-    screen = turtle.Screen()
-    try:
-        screen.bgpic(f"./Backgrounds/{image_filename}.png")
-    except:
-        screen.bgpic(f"./Backgrounds/{image_filename}.gif")
-
-def set_image(sprite, image_filename):
-    image_file = f"./Images/{image_filename}.gif"
-    screen = turtle.Screen()
-    screen.register_shape(image_file)
-    sprite.shape(image_file)
-
-def create_sprite(image_filename, x=0, y=0):
-    sprite = turtle.Turtle()
-    set_image(sprite, image_filename)
-    sprite.penup()
-    sprite.goto(x,y)
-    window.update()
-    return sprite
-
-def get_distance(s1, s2):
-    dx = s1.xcor() - s2.xcor()
-    dy = s1.ycor() - s2.ycor()
-    return math.sqrt(dx*dx + dy*dy)
-
-window = turtle.Screen()
-window.tracer(0)
+from utils import *
 
 # Section 2 - setup
 set_background("castle")
@@ -68,7 +41,7 @@ while True:
     # Automatically create basketballs every 1 seconds
     if timer % 10 == 0:
         y_position = random.randint(-250, 250)
-        s2 = create_sprite("basketball",300,y_position)
+        s2 = create_sprite("basketball.gif",300,y_position)
         s2.setheading(180)
         obstacles.append(s2)
         
@@ -80,6 +53,7 @@ while True:
         if get_distance(s1,s2) < 50:
             lives -= 1
             s2.hideturtle()
+            set_image(s1, "puppy")
             obstacles.remove(s2)
 
 
