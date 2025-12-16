b6005b59331608b64c7c325f0228e6629fda4be4
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2025-12-16 09:50:39 -0800

    update coat of arms example

--- Projects/p3_coat_of_arms_pygame_example_code.py
+++ Projects/p3_coat_of_arms_pygame_example_code.py
@@ -1,65 +1,11 @@
-# Section 1 - Helper functions (DON'T CHANGE!!)
-import turtle, math, time, random
-def set_background(image_filename):
-	screen = turtle.Screen()
-	try:
-		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
-	except:
-		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
-
-def set_image(sprite, image_filename):
-    image_file = f"./Images/{image_filename}.gif"
-    screen = turtle.Screen()
-    screen.register_shape(image_file)
-    sprite.shape(image_file)
-
-def create_sprite(image_filename, x=0, y=0):
-    sprite = turtle.Turtle()
-    set_image(sprite, image_filename)
-    sprite.penup()
-    sprite.goto(x,y)
-    window.update()
-    return sprite
-
-def get_distance(s1, s2):
-	dx = s1.xcor() - s2.xcor()
-	dy = s1.ycor() - s2.ycor()
-	return math.sqrt(dx*dx + dy*dy)
-
-def draw_rectangle( color="black",x=0,y=0, width=100, height=100,):
-	sprite = turtle.Turtle()
-	sprite.speed(0)
-	sprite.pencolor(color)
-	sprite.color(color)
-	sprite.penup()
-	sprite.goto(x - (width*0.5), y + (height*0.5))
-	sprite.pendown()
-	sprite.begin_fill()
-	for i in range(2):
-		sprite.forward(width)
-		sprite.right(90)
-		sprite.forward(height)
-		sprite.right(90)
-	sprite.end_fill()
-	sprite.hideturtle()
-
-
-window = turtle.Screen()
-window.tracer(0)
-
-
-
-######################################################################
-# https://en.wikipedia.org/wiki/Web_colors#Extended_colors
-# Section 2 - Your code
-set_background("fall")
-
-draw_rectangle("black", 100, 100, 200, 200)
-draw_rectangle("red", -100, 100, 200, 200)
-draw_rectangle("red", 100, -100, 200, 200)
+# Section 1 - Your code
+from utils import *
+set_background("winter")
 
 s1 = create_sprite("cardinal", 100, 100)
+s2 = create_sprite("cardinal", -100, 100)
 s2 = create_sprite("cardinal", -100, -100)
+s2 = create_sprite("cardinal", 100, -100)
 
 message1 = create_sprite("alien",-200,200)
 message1.color("red")
@@ -70,6 +16,6 @@ message1.hideturtle()
 ######################################################################
 
 
-# Section 3 - Keeping the window open (DON'T CHANGE!!)
+# Section 2 - Keeping the window open (DON'T CHANGE!!)
 window.update()
 turtle.exitonclick()
