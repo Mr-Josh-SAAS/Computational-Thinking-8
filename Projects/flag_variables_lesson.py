b82ae3ca7df06b6aab7663f5772d685d709fcded
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2025-12-18 15:12:02 -0800

    update flag lesson to 2025

--- Projects/flag_variables_lesson.py
+++ Projects/flag_variables_lesson.py
@@ -5,9 +5,6 @@ t = turtle.Turtle()
 t.speed(10)
 turtle.Screen().bgcolor("light blue")
 
-height = 10
-# stripes
-
 # move to stripe 1
 t.goto(-250, -100)
 
@@ -16,76 +13,11 @@ t.color("red")
 t.begin_fill()
 t.forward(500)
 t.left(90)
-t.forward(height)
+t.forward(50)
 t.left(90)
 t.forward(500)
 t.left(90)
-t.forward(height)
-t.left(90)
-t.end_fill()
-
-
-# move to stripe 2
-t.goto(-250, -50)
-
-# stripe 2
-t.color("white")
-t.begin_fill()
-t.forward(500)
-t.left(90)
-t.forward(height)
-t.left(90)
-t.forward(500)
-t.left(90)
-t.forward(height)
-t.left(90)
-t.end_fill()
-
-# move to stripe 3
-t.goto(-250, 0)
-
-# stripe 3
-t.color("red")
-t.begin_fill()
-t.forward(500)
-t.left(90)
-t.forward(height)
-t.left(90)
-t.forward(500)
-t.left(90)
-t.forward(height)
-t.left(90)
-t.end_fill()
-
-# move to stripe 4
-t.goto(-250, 50)
-
-# stripe 4
-t.color("white")
-t.begin_fill()
-t.forward(500)
-t.left(90)
-t.forward(height)
-t.left(90)
-t.forward(500)
-t.left(90)
-t.forward(height)
-t.left(90)
-t.end_fill()
-
-# move to stripe 5
-t.goto(-250, 100)
-
-# stripe 5
-t.color("red")
-t.begin_fill()
-t.forward(500)
-t.left(90)
-t.forward(height)
-t.left(90)
-t.forward(500)
-t.left(90)
-t.forward(height)
+t.forward(50)
 t.left(90)
 t.end_fill()
 
