0a71f809a3294235214e53cc7c5e26915e8a71dc
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2026-01-12 10:01:59 -0800

    update coat of arms for an activity, not project

---
+++ Projects/coat_of_arms_Josh.py
@@ -0,0 +1,28 @@
+# Section 1 - Your code
+from utils import *
+set_background("winter")
+
+s0 = create_sprite("coa_grid",0,0)
+s1 = create_sprite("computer2", 100, 100)
+s2 = create_sprite("knitting2", -100, -100)
+s3 = create_sprite("rock_climbers2", 100, -100)
+s4 = create_sprite("cardinal", -100, 100)
+
+message1 = create_sprite("alien",-180,220)
+message1.color("black")
+message1.write("Josh Kurisko",font = ("Arial", 40, "normal"))
+message1.hideturtle()
+
+message2 = create_sprite("alien",-160,-280)
+message2.color("SkyBlue")
+message2.write("Python is fun!",font = ("Arial", 40, "normal"))
+message2.hideturtle()
+
+
+
+######################################################################
+
+
+# Section 2 - Keeping the window open (DON'T CHANGE!!)
+window.update()
+turtle.exitonclick()
--- Projects/coat_of_arms_example.py
+++ Projects/coat_of_arms_example.py
@@ -4,13 +4,18 @@ set_background("winter")
 
 s1 = create_sprite("cardinal", 100, 100)
 s2 = create_sprite("cardinal", -100, 100)
-s2 = create_sprite("cardinal", -100, -100)
-s2 = create_sprite("cardinal", 100, -100)
+s3 = create_sprite("cardinal", -100, -100)
+s4 = create_sprite("cardinal", 100, -100)
 
 message1 = create_sprite("alien",-200,200)
 message1.color("red")
 message1.write("Your Name",font = ("Arial", 40, "normal"))
 message1.hideturtle()
+
+message2 = create_sprite("alien",-200,-250)
+message2.color("black")
+message2.write("Your motto",font = ("Arial", 40, "normal"))
+message2.hideturtle()
 
 
 ######################################################################
