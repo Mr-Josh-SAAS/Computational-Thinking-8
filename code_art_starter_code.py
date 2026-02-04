698c5ff6468c1e6bc92cc8123ce569e345bd5ea4
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2026-02-04 14:23:23 -0800

    massively prune files, match filenames with study skills branch

---
+++ code_art_starter_code.py
@@ -0,0 +1,27 @@
+# ###############################################
+# ### SETUP ###
+import turtle
+# ###############################################
+
+t = turtle.Turtle()
+t.penup()
+t.goto(-100, -100)
+t.color("red")
+t.pendown()
+
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(100)
+t.left(72)
+t.forward(50)
+
+
+
+# ###############################################
+# ### ENDING ###
+turtle.exitonclick()
+# ###############################################
