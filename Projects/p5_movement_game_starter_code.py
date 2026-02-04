42561befb229761cac89be8f04e8b6d040977cba
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2026-02-04 11:27:00 -0800

    streamline P5 starter code and add tag example

--- Projects/p5_movement_game_starter_code.py
+++ Projects/p5_movement_game_starter_code.py
@@ -1,81 +1,32 @@
-# Section 1 - Helper functions (DON'T CHANGE!!)
-import turtle, math, time, random
-def set_background(image_filename):
-	screen = turtle.Screen()
-	try:
-		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.png")
-	except:
-		screen.bgpic(f"/workspaces/Computational-Thinking-8/Backgrounds/{image_filename}.gif")
+import turtle
+import math
+import time
+import random
+from utils import *
 
-def set_image(sprite, image_filename):
-    image_file = f"./Images/{image_filename}.gif"
-    screen = turtle.Screen()
-    screen.register_shape(image_file)
-    sprite.shape(image_file)
-
-def create_sprite(image_filename, x=0, y=0):
-    sprite = turtle.Turtle()
-    set_image(sprite, image_filename)
-    sprite.penup()
-    sprite.goto(x,y)
-    window.update()
-    return sprite
-
-def get_distance(s1, s2):
-	dx = s1.xcor() - s2.xcor()
-	dy = s1.ycor() - s2.ycor()
-	return math.sqrt(dx*dx + dy*dy)
-
-def draw_rectangle( color="black",x=0,y=0, width=100, height=100,):
-	sprite = turtle.Turtle()
-	sprite.speed(0)
-	sprite.pencolor(color)
-	sprite.color(color)
-	sprite.penup()
-	sprite.goto(x - (width*0.5), y + (height*0.5))
-	sprite.pendown()
-	sprite.begin_fill()
-	for i in range(2):
-		sprite.forward(width)
-		sprite.right(90)
-		sprite.forward(height)
-		sprite.right(90)
-	sprite.end_fill()
-	sprite.hideturtle()
-
-
-window = turtle.Screen()
-window.tracer(0)
-
-
-# Section 2: Setup
+# Section 1: Setup
 # TODO - create your player character
 # TODO - set your background
 # TODO - set the starting value for your variable
+sprite_list = []
 
-# Section 3: Controls
+# Section 2: Controls
 # TODO - define your controls
 # TODO - pick keys for each control
 
-# Section 4: Game Loop
+# Section 3: Game Loop
 window.listen()
-timer = 0
-while True:
-	time.sleep(0.1)
-	timer += 1  
-	 
+for i in range(10000000000):
     
- 	# TODO - code for automatic actions
+    # TODO - add code for automatic actions
+            
+    
+	# TODO - make an if statement for ending the game
 
 
+    time.sleep(0.01)
+    window.update()
+    
 
-
-
-
-	window.update()
-
-	# if :
-	# 	break
 	
-
 print("Game Over")
