7ab2d3141fff6aa998d3bbf65e0b97ad509c07d9
Mr-Josh-SAAS <jkurisko@seattleacademy.org>
2026-03-25 07:54:06 -0700

    try an animated conversation

---
+++ animated_conversation.py
@@ -0,0 +1,42 @@
+# Section 1 - Your code
+from utils import *
+player_name = input("What is your name?    ")
+
+set_background("moon")
+
+s1 = create_sprite("fish", -200, 0)
+s2 = create_sprite("stitch", 200, 0)
+
+s1.color("blue")
+s2.color("dark red")
+time.sleep(5)
+
+s1.write("Where are we?",font = ("Arial", 20, "normal"))
+window.update()
+time.sleep(1)
+
+s1.clear()
+window.update()
+time.sleep(1)
+
+s2.write("On the moon!",font = ("Arial", 20, "normal"))
+window.update()
+time.sleep(1)
+
+s2.clear()
+window.update()
+time.sleep(1)
+
+s1.write(f"I'm looking for a lost fish named {player_name}",font = ("Arial", 20, "normal"))
+window.update()
+time.sleep(1)
+
+s1.clear()
+s1.write("Have you seen one?",font = ("Arial", 20, "normal"))
+window.update()
+time.sleep(1)
+
+######################################################################
+# Section 2 - Keeping the window open (DON'T CHANGE!!)
+window.update()
+turtle.exitonclick()
